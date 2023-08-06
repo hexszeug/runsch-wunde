29f6ccf3baa05af233644f4663acaf4d7cbe00b6
Siemen Zielke <77232580+hexszeug@users.noreply.github.com>
2023-08-06 11:07:11 +0200

    color scheme

--- runsch-wunde-react/src/App.js
+++ runsch-wunde-react/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="hero is-medium">
+        <div className="hero-body">
+          <h1 className="title">Hello World</h1>
+        </div>
+      </div>
+      <div className="container">
+        <div className="field">
+          <label className="label">Search</label>
+          <div className="control">
+            <input className="input" type="text" placeholder="Typing..." />
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column">
+            <button className="button is-primary">Primary</button>
+          </div>
+          <div className="column">
+            <button className="button is-link">Link</button>
+          </div>
+          <div className="column">
+            <button className="button is-info">Info</button>
+          </div>
+          <div className="column">
+            <button className="button is-success">Success</button>
+          </div>
+          <div className="column">
+            <button className="button is-warning">Warning</button>
+          </div>
+          <div className="column">
+            <button className="button is-danger">Danger</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
