71e368df0af3956362a251f421be3e20b7a33c45
Siemen Zielke <77232580+hexszeug@users.noreply.github.com>
2023-08-16 20:25:23 +0200

    message to notify host on login screen

--- runsch-wunde-react/src/Login.js
+++ runsch-wunde-react/src/Login.js
@@ -4,6 +4,13 @@ import spotifyLogo from './assets/spotify/logos/Spotify_Logo_RGB_White.png';
 const Login = () => {
   return (
     <div>
+      <section className="section">
+        <div className="container">
+          <h1 className="title has-text-centered">
+            Bitte benachrichtige den Gastgeber
+          </h1>
+        </div>
+      </section>
       <div className="buttons is-centered">
         <button
           className="button is-large is-primary"
